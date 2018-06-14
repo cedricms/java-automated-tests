34f12fa5965e0fbd7a4dce328d11efb24a4390fc
Cédric Maria-Sube <cedric@maria-sube.com>
2018-06-14 09:49:42 +0000

    Adds the basic classes for the tests on Fibonacci.

---
+++ src/main/java/com/automated/tests/unit/fibonacci/Fibonacci.java
@@ -0,0 +1,12 @@
+package com.automated.tests.unit.fibonacci;
+
+public class Fibonacci {
+
+    public Fibonacci() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Fibonacci!");
+    }
+}
---
+++ src/test/java/com/automated/tests/unit/fibonacci/FibonacciUnitTest.java
@@ -0,0 +1,11 @@
+package com.automated.tests.unit.fibonacci;
+
+import org.junit.Test;
+
+public class FibonacciUnitTest {
+
+    @Test
+     public void methodeDeTestARemplacer() {
+
+    }
+}
